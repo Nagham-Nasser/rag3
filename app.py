5ef821b837285a3475c57f1995ee5f063c98b97c
Nagham Nasser <144176670+Nagham-Nasser@users.noreply.github.com>
2025-02-11 16:41:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_community.llms import HuggingFaceLLM
+from langchain_huggingface import HuggingFaceLLM
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
